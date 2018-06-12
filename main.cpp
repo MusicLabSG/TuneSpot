82b3ddc7f86603593f3d9c41c532ac92377fd860
spyridon97 <spyridon97@hotmail.com>
2018-06-12 18:25:11 +0300

    finished change of frequency

--- main.cpp
+++ main.cpp
@@ -7,12 +7,8 @@ int main(int argc, char *argv[]) {
 
     QApplication app(argc, argv);
 
-    Configurator *configurator = new Configurator();
-    qInfo() << configurator->getBaseFrequency();
-    qInfo() << configurator->getClosestNote();
-    configurator->setCelloXString(1);
-    qInfo() << configurator->getPercentageOfDistanceFromTheClosestNote();
-
+    // Register the backend as a component
+    qmlRegisterType<Configurator>("com.github.tune_spot.configurator", 1, 0, "Configurator");
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
