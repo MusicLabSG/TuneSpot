973e6ac93c152bf4c3fdc2cc7b1e22996da0695e
Theodoros Bozinis <mpozinit@csd.auth.gr>
2018-06-14 14:44:50 +0300

    implimented some methods

--- core.cpp
+++ core.cpp
@@ -5,7 +5,38 @@ Core::Core(QObject *parent) : QObject(parent)
 
 }
 
+void Core::setInstrument(QString instrument)
+{
+    this->instrument = instrument;
+}
+
+void Core::setString(qint16 string)
+{
+//    TODO check for strcpy
+    this->string = string;
+}
+
+void Core::setBaseFrequency(qint16 frequency)
+{
+    configurator.changeBaseFrequency(frequency);
+}
+
+qint16 Core::getBaseFrequency()
+{
+    return configurator.getBaseFrequency();
+}
+
+qint16 Core::getPercentage()
+{
+    return configurator.getPercentageOfDistanceFromTheClosestNote();
+}
+
+QString Core::getClosestNote()
+{
+    return configurator.getClosestNote();
+}
+
 void Core::emitResults()
 {
-    emit onFrequencyResult(123);
+    emit frequencyResult(123);
 }
