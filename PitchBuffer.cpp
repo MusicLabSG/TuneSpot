411fe15da1d81e0e798ee1d98507a0019127c3e3
spyridon97 <spyridon97@hotmail.com>
2018-06-18 20:19:02 +0300

    possibly working backend

---
+++ PitchBuffer.cpp
@@ -0,0 +1,69 @@
+/*
+ * File:    PitchBuffer.cpp
+ * Author:  Spiros
+ *
+ * Created on Jun 16, 2018, 5:28 PM
+ */
+
+#include <QDebug>
+#include <PitchBuffer.hpp>
+
+PitchBuffer::PitchBuffer(QObject *parent) : QIODevice(parent) {
+
+}
+
+bool PitchBuffer::getSamples(fvec_t *samplesDest) {
+    if (m_buf.size() < HOP_SIZE) {
+        return false;
+    }
+
+    for (int i = 0; i < HOP_SIZE; ++i) {
+        fvec_set_sample(samplesDest, m_buf.front(), i);
+        m_buf.pop();
+    }
+
+    return true;
+}
+
+void PitchBuffer::setSampleType(QAudioFormat::SampleType type, int size) {
+    m_sampleType = type;
+    m_sampleSize = size;
+}
+
+qint64 PitchBuffer::readData(char *data, qint64 maxlen) {
+    Q_UNUSED(data)
+    Q_UNUSED(maxlen)
+
+    return -1;
+}
+
+qint64 PitchBuffer::writeData(const char *data, qint64 len) {
+    auto samples = getFloatSamples(data, len);
+    const int numberNewSamples = samples.size();
+
+    for (int i = 0; i < numberNewSamples; ++i) {
+        const float sample = samples[i];
+        m_buf.push(sample);
+    }
+
+    if (m_buf.size() >= HOP_SIZE) {
+        emit samplesReady();
+    }
+
+    return len;
+}
+
+std::vector<float> PitchBuffer::getFloatSamples(const char *data, qint64 len) {
+    const unsigned char *ptr = reinterpret_cast<const unsigned char *>(data);
+    std::vector<float> result;
+    const int numberSamples = len / m_sampleSize;
+    result.resize(numberSamples);
+    for (int i = 0; i < numberSamples; ++i) {
+        float sample = *reinterpret_cast<const float *>(ptr);;
+
+        result[i] = sample;
+        ptr += m_sampleSize;
+    }
+
+    return result;
+}
